69bfe775a29c5e5daf4ea3752794335077f58ef1
Carlos Ariza <caticoa3@gmail.com>
2021-03-03 16:13:46 -0800

    bug fix: save html version of notebook only when jupytext is saving *.ipynb

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,14 +1,15 @@
 c = get_config()
-### If you want to auto-save .html and .py versions of your notebook:
 # modified from: https://github.com/ipython/ipython/issues/8009
 import os
 from subprocess import check_call
 
 def post_save(model, os_path, contents_manager):
-    """post-save hook for converting notebooks to .py scripts"""
-    if model['type'] != 'notebook':
-        return # only do this for notebooks
+    """post-save hook for converting notebooks to .html"""
     d, fname = os.path.split(os_path)
+
+    if (model['type'] != 'notebook') or (fname.split('.')[1] != 'ipynb'):
+        return # only do this for notebooks and avoid when jupytext saves a *.py 
+
     check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
 
 c.FileContentsManager.post_save_hook = post_save
