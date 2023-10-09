262daa0768b7fb5fd9b66e61dfd1108242e14749
caticoa3 <caticoa3@users.noreply.github.com>
2023-10-08 23:39:20 -0700

    working python black formatter

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -14,8 +14,9 @@ null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.stylua,
         --null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+        --null_ls.builtins.completion.spell,
     },
 })
