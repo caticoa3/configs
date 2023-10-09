e68d59f142656ec6647483ac07090dfc51851c6e
caticoa3 <caticoa3@users.noreply.github.com>
2023-10-08 23:21:43 -0700

    working config for none-ls

---
+++ .config/nvim/lua/custom/none-ls.lua
@@ -0,0 +1,21 @@
+--[[ local none_ls = require('none-ls') ]]
+
+--[[ local opts = {
+   [     sources = {
+   [         none_ls.builtins.diagnostics.mypy,
+   [         none_ls.builtins.diagnostics.ruff,
+   [     }
+   [ }
+   [ return opts ]]
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.formatting.stylua,
+        --null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+    },
+})
