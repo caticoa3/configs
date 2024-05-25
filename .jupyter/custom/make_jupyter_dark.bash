43c411fdf3570caf30616ad60b7f3bdfc6cf5b3e
Carlos Ariza <caticoa3@gmail.com>
2024-05-24 17:00:22 -0700

    correct errors with double quote in bash scripts

--- .jupyter/custom/make_jupyter_dark.bash
+++ .jupyter/custom/make_jupyter_dark.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd $HOME||exit
-source $HOME/anaconda3/bin/activate 
+cd "$HOME"||exit
+source "$HOME/anaconda3/bin/activate"
 conda activate 
 make jupyter_dark
-source $HOME/anaconda3/bin/deactivate
+source "$HOME/anaconda3/bin/deactivate"
--- .jupyter/custom/make_jupyter_light.bash
+++ .jupyter/custom/make_jupyter_light.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd $HOME||exit
-source $HOME/anaconda3/bin/activate 
+cd "$HOME"||exit
+source "$HOME/anaconda3/bin/activate"
 conda activate 
 make jupyter_light
-source $HOME/anaconda3/bin/deactivate
+source "$HOME/anaconda3/bin/deactivate"
