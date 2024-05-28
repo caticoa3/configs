27567435808e0726372e6a878bbfafc9ca93c621
caticoa3 <5348361+caticoa3@users.noreply.github.com>
2024-05-28 06:07:40 +0000

    [MegaLinter] Apply linters automatic fixes

--- .jupyter/custom/make_jupyter_light.bash
+++ .jupyter/custom/make_jupyter_light.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd "$HOME"||exit
+cd "$HOME" || exit
 source "$HOME/anaconda3/bin/activate"
-conda activate 
+conda activate
 make jupyter_light
 source "$HOME/anaconda3/bin/deactivate"
