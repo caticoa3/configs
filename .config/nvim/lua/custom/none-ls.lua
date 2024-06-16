cb2cec911f2a9c862e2880aff6ea06c0f30f1836
Carlos Ariza <caticoa3@gmail.com>
2024-06-16 11:52:58 -0700

    remove duplicated mypy in none-ls config

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -1,25 +1,24 @@
 local null_ls = require("null-ls")
+print("testing printing in none-ls.lua")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.mypy.with({
         extra_args = function()
-        --[[ local virtual = os.getenv("VIRTUAL_ENV") or "/usr" ]]
-        if os.getenv("CONDA_PREFIX") then 
-            -- conda env is active
-            local anaconda_path = os.getenv("HOME") .. "/anaconda3/envs/" .. os.getenv("CONDA_DEFAULT_ENV")
-            print(anaconda_path)
-            return {"--python-executable", anaconda_path  .. "/bin/python"} --,  "--explicit-package-bases", anaconda_path}
-        elseif os.getenv("VIRTUAL_ENV") then
-            print(os.getenv("VIRTUAL_ENV"))
-            return {"--python-executable", os.getenv("VIRTUAL_ENV") .. "/bin/python"}
+            if os.getenv("CONDA_PREFIX") then -- conda env is active
+                local anaconda_path = os.getenv("HOME") .. "/anaconda3/envs/" .. os.getenv("CONDA_DEFAULT_ENV")
+                print(anaconda_path)
+                return {"--python-executable", anaconda_path  .. "/bin/python"} --,  "--explicit-package-bases", anaconda_path}
+            elseif os.getenv("VIRTUAL_ENV") then
+                print(os.getenv("VIRTUAL_ENV"))
+                return {"--python-executable", os.getenv("VIRTUAL_ENV") .. "/bin/python"}
+            end
         end
-        end,
         }),
-        --[[ null_ls.builtins.diagnostics.ruff, ]]
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.proselint,
+        --null_ls.builtins.diagnostics.ruff,
         --null_ls.builtins.diagnostics.eslint,
         --null_ls.builtins.completion.spell,
     },
