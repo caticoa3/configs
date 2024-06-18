96331f0dda97ef1c6be0f1e1543b44b9d41ba9c4
Carlos Ariza <caticoa3@gmail.com>
2024-06-18 16:17:34 -0700

    detecting and indicating virtual environment in statusline

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -1,23 +1,20 @@
 local null_ls = require("null-ls")
-print("testing printing in none-ls.lua")
 
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.mypy.with({
-        extra_args = function()
-            if os.getenv("CONDA_PREFIX") then -- conda env is active
-                local anaconda_path = os.getenv("HOME") .. "/anaconda3/envs/" .. os.getenv("CONDA_DEFAULT_ENV")
-                print(anaconda_path)
-                return {"--python-executable", anaconda_path  .. "/bin/python"} --,  "--explicit-package-bases", anaconda_path}
-            elseif os.getenv("VIRTUAL_ENV") then
-                print(os.getenv("VIRTUAL_ENV"))
-                return {"--python-executable", os.getenv("VIRTUAL_ENV") .. "/bin/python"}
-            end
-        end
+	-- each python environment should have mypy installed
+        -- don't run without vim.g.python3_host_prog environment; set in init.lua 
+        condition = function() return vim.g.python3_host_prog ~= nil end,
+        --[[extra_args = function(_)
+           [    if vim.g.python3_host_prog ~= nil then
+           [        return "--python-executable " .. vim.g.python3_host_prog
+           [    end
+           [end]]
         }),
-        null_ls.builtins.formatting.black,
+        --[[null_ls.builtins.formatting.black,]]
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.proselint,
+        --[[null_ls.builtins.code_actions.proselint,]]
         --null_ls.builtins.diagnostics.ruff,
         --null_ls.builtins.diagnostics.eslint,
         --null_ls.builtins.completion.spell,
