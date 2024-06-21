aeabbb85927fd1bf371ff8e7ac8dde885166abd8
Carlos Ariza <caticoa3@gmail.com>
2024-06-20 22:53:39 -0700

    troubleshot mypy in none-ls increase timeout for complex scripts

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -1,22 +1,24 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
+    default_timeout = 30000,
+    debug = true,
     sources = {
         null_ls.builtins.diagnostics.mypy.with({
 	-- each python environment should have mypy installed
         -- don't run without vim.g.python3_host_prog environment; set in init.lua 
         condition = function() return vim.g.python3_host_prog ~= nil end,
-        --[[extra_args = function(_)
+        --[[extra_args = function()
            [    if vim.g.python3_host_prog ~= nil then
            [        return "--python-executable " .. vim.g.python3_host_prog
            [    end
            [end]]
         }),
-        --[[null_ls.builtins.formatting.black,]]
+        --null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.stylua,
-        --[[null_ls.builtins.code_actions.proselint,]]
+        --null_ls.builtins.code_actions.proselint,
         --null_ls.builtins.diagnostics.ruff,
         --null_ls.builtins.diagnostics.eslint,
         --null_ls.builtins.completion.spell,
-    },
+    }
 })
