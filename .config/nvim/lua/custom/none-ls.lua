8e967dbafd2398b874bc35a7434422f16832130c
caticoa3 <caticoa3@users.noreply.github.com>
2024-06-26 22:27:03 -0700

    no timeout for mypy

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -1,13 +1,21 @@
 local null_ls = require("null-ls")
+--local utils = require("null-ls.utils")
+--local d = utils.root_pattern("mypy.ini")
+
+--for key,value in pairs(d) do
+--    print('k'..key..'value'..value)
+--end
 
 null_ls.setup({
-    default_timeout = 30000,
     debug = true,
     sources = {
         null_ls.builtins.diagnostics.mypy.with({
 	-- each python environment should have mypy installed
         -- don't run without vim.g.python3_host_prog environment; set in init.lua 
         condition = function() return vim.g.python3_host_prog ~= nil end,
+        -- milliseconds
+        timeout = -1, --a negavite number prevents timeout
+        --multiple_files = false,
         --[[extra_args = function()
            [    if vim.g.python3_host_prog ~= nil then
            [        return "--python-executable " .. vim.g.python3_host_prog
