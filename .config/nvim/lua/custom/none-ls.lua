5cc4184a36e3ed8563d09668f4dc26b6d8480919
Carlos Ariza <caticoa3@gmail.com>
2025-04-08 22:54:48 -0700

    small tweaks to nvim config trying out a few packages. e.g autoparing {}

--- .config/nvim/lua/custom/none-ls.lua
+++ .config/nvim/lua/custom/none-ls.lua
@@ -9,23 +9,13 @@ local null_ls = require("null-ls")
 null_ls.setup({
     debug = true,
     sources = {
-        null_ls.builtins.diagnostics.mypy.with({
-	-- each python environment should have mypy installed
-        -- don't run without vim.g.python3_host_prog environment; set in init.lua 
-        condition = function() return vim.g.python3_host_prog ~= nil end,
-        -- milliseconds
-        timeout = -1, --a negavite number prevents timeout
-        --multiple_files = false,
-        --[[extra_args = function()
-           [    if vim.g.python3_host_prog ~= nil then
-           [        return "--python-executable " .. vim.g.python3_host_prog
-           [    end
-           [end]]
-        }),
-        --null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.stylua,
+        -- Python formatting
+        null_ls.builtins.formatting.black,
+        -- Python linting
+        null_ls.builtins.diagnostics.ruff,
+        --null_ls.builtins.diagnostics.mypy,
+        --null_ls.builtins.formatting.stylua,
         --null_ls.builtins.code_actions.proselint,
-        --null_ls.builtins.diagnostics.ruff,
         --null_ls.builtins.diagnostics.eslint,
         --null_ls.builtins.completion.spell,
     }
